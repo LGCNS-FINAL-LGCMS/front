e2b00394126d5df07c1197e10d0b9938452ff6ea
Lookies09 <edcvfr40679051@gmail.com>
2025-07-24 15:57:40 +0900

    [LF-80] feat : 알림 셀

---
+++ src/assets/styles/theme.ts
@@ -0,0 +1,17 @@
+import type { DefaultTheme } from "styled-components";
+
+export const theme: DefaultTheme = {
+  colors: {
+    primary: "#828282",
+    secondary: "#FFDD9E",
+    danger: "#dc3545",
+    background_D: "#212529",
+    background_B: "rgba(240, 240, 240, 1)",
+    header: "#000000a7",
+    text_D: "#212529", // 어두운 텍스트_Dark
+    text_B: "#f2f2f2", // 밝은 텍스트_Bright
+  },
+  size: {
+    maxWidth: "1200px",
+  },
+};
