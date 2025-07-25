72c5a9b9a343388b09f945b8295ccc2c2822993c
Lookies09 <edcvfr40679051@gmail.com>
2025-07-25 15:24:52 +0900

    [LF-93] feat : 강의 카드

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import styled from "styled-components";
 import Button from "../components/Common/Button";
 import SearchBar from "../components/Common/SearchBar";
+import LectureCard from "../components/Common/LectureCard";
 
 const Title = styled.h1`
   color: ${({ theme }) => theme.colors.caution};
@@ -32,16 +33,15 @@ const MainPage = () => {
 
   return (
     <Container>
-      <SearchBar
-        value={searchValue}
-        fontColor={1}
-        fontWeight={100}
-        design={2}
-        width="450px"
-        onChange={handleInputChange}
-        onSearch={handleSearch}
-        disabled={false}
-        placeholder="하이요"
+      <LectureCard
+        imageUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTSBwCciH8r1Ae27FRiVp4GmcH510aLHssTrw&s"
+        title="Next.js 완벽 정복"
+        description="Next.js와 React를 활용한 SSR 웹dddddddd "
+        lecturer="홍길동"
+        design={3}
+        fontColor={2}
+        width="250px"
+        onClick={() => alert("강의 클릭됨!")}
       />
     </Container>
   );
