a46a7c08dba97211ef58ec0b36b95acd774abc2a
Imdahee <ldhdh1832@gmail.com>
2025-08-08 15:27:32 +0900

    [LF-42]feat: 로그인 후 회원/비회원 이동구현

--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -4,35 +4,36 @@ import styled from "styled-components";
 
 const LoginBoxContainer = styled.div`
   display: flex;
-  justify-content: center; /* 가로 중앙 정렬 */
-  align-items: center; /* 세로 중앙 정렬 */
+  justify-content: center; // 가로 중앙 정렬
+  align-items: center; //세로 중앙 정렬
   min-height: 80vh;
 `;
 
 const LoginCard = styled.div`
   background: white;
-  border: 2px solid ${(props) => props.theme.colors.border_Dark}; /* 테마 색상 사용 */
+  border: 2px solid ${(props) => props.theme.colors.border_Dark};
   border-radius: 16px;
-  padding: 60px 60px;
+  padding: 20px 60px;
   text-align: center;
 
   width: 600px;
-  margin: 0 auto;
+  height: 400px;
 `;
 
 const Title = styled.h1`
   font-family: ${(props) => props.theme.font.logo};
+  padding: 30px 60px;
 
   font-size: 48px;
   font-weight: bold;
   color: #333;
-  margin: 0 0 30px 0;
   letter-spacing: 2px;
 `;
 
 const Subtitle = styled.h3`
+  padding: 20px 60px;
+
   font-family: ${(props) => props.theme.font.primary};
-  margin-bottom: 40px;
 `;
 
 const Login = () => {
