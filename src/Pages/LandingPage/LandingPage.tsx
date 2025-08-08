e8d09caf214ecbb22f93500a88599fd1c6b92b35
Lookies09 <edcvfr40679051@gmail.com>
2025-08-08 20:21:25 +0900

    [LF-186] feat : 랜딩페이지 구성

---
+++ src/Pages/LandingPage/LandingPage.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Spline from "@splinetool/react-spline";
+
+const LandingPage = () => {
+  return (
+    <Spline scene="https://prod.spline.design/8DAehG0qPG3zxABM/scene.splinecode" />
+  );
+};
+
+export default LandingPage;
