67061d7f292e5c3a9fe45e9ed12530b1ff1ac3d2
Imdahee <ldhdh1832@gmail.com>
2025-08-12 09:52:34 +0900

    [LF-41]fix: 닉네임 중복확인 api 중복호출 방치

--- src/pages/Signup/SignupPage.tsx
+++ src/pages/Signup/SignupPage.tsx
@@ -82,7 +82,8 @@ const SignupPage = () => {
 
   const [nickname, setNickname] = useState(""); //input에서 받은 nickname
   const [nicknameCheckMessage, setNicknameCheckMessage] = useState(""); // 중복확인 결과 메세지
-  const [nicknameCheck, setNicknameCheck] = useState<boolean | null>(null); // 회원가입완료 시 중복확인 검사
+  const [nicknameCheck, setNicknameCheck] = useState<boolean | null>(null); // 회원가입완료 시 중복확인 검사여부
+  const [lastNickname, setLastNickname] = useState(""); // api중복으로 안보내게
 
   const [selectedCategories, setSelectedCategories] = useState<
     CategoryFormat[]
@@ -96,6 +97,7 @@ const SignupPage = () => {
     setNickname(e.target.value);
     setNicknameCheck(null); // input 바뀌면 중복확인 다시해야됨
     setNicknameCheckMessage("");
+    setLastNickname("");
   };
 
   //중복버튼 클릭 시
@@ -103,21 +105,29 @@ const SignupPage = () => {
     if (nickname === "") {
       setNicknameCheckMessage("닉네임을 입력해주세요.");
       return;
-    } else {
-      try {
-        const result = await checkNicknameDuplicate(nickname);
-        console.log(result.data.isUsed);
-        if (result.data.isUsed === false) {
-          setNicknameCheck(false);
-          setNicknameCheckMessage("사용할 수 없는 닉네임입니다.");
-        } else if (result.data.isUsed === true) {
-          setNicknameCheck(true);
-          setNicknameCheckMessage("사용가능한 닉네임입니다.");
-        }
-      } catch (error) {
-        console.error("중복확인 오류:", error);
-        setNicknameCheckMessage("오류가 발생했습니다. 다시 시도해주세요.");
+    } // 마지막 쓴 nickname과 같고, 체크도 되어있을 때
+    if (nickname === lastNickname && nicknameCheck !== null) {
+      setNicknameCheckMessage(
+        nicknameCheck
+          ? "사용가능한 닉네임입니다."
+          : "사용할 수 없는 닉네임입니다."
+      );
+      return;
+    }
+    try {
+      const result = await checkNicknameDuplicate(nickname);
+      console.log(result.data.isUsed);
+      setLastNickname(nickname);
+      if (result.data.isUsed === false) {
+        setNicknameCheck(false);
+        setNicknameCheckMessage("사용할 수 없는 닉네임입니다.");
+      } else if (result.data.isUsed === true) {
+        setNicknameCheck(true);
+        setNicknameCheckMessage("사용가능한 닉네임입니다.");
       }
+    } catch (error) {
+      console.error("중복확인 오류:", error);
+      setNicknameCheckMessage("오류가 발생했습니다. 다시 시도해주세요.");
     }
   };
 
