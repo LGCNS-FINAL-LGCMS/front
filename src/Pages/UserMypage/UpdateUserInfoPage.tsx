5611131a755accc813b070bfb2580f04d7f9053c
Imdahee <ldhdh1832@gmail.com>
2025-08-18 19:40:00 +0900

    [LF-84]feat: 회원가입수정 페이지 기능구현

--- src/Pages/UserMypage/UpdateUserInfoPage.tsx
+++ src/Pages/UserMypage/UpdateUserInfoPage.tsx
@@ -1,13 +1,17 @@
 import styled from "styled-components";
-import {
-  checkNicknameDuplicate,
-  signupAPI,
-  type CategoryFormat,
-} from "../../api/Signup/signupAPI";
-import Button from "../../components/Common/Button";
-
 import { useState, useCallback } from "react";
+import { useNavigate } from "react-router-dom";
+import { PAGE_PATHS } from "../../constants/pagePaths";
+import { useDispatch } from "react-redux";
+import type { AppDispatch } from "../../redux/store";
+
+import { checkNicknameAPI, signupAPI } from "../../api/Signup/signupAPI";
+import type { UserCategoriesList } from "../../types/authInfo";
+import Button from "../../components/Common/Button";
 import CategorySelect from "../../components/Signup/CategorySelect";
+import InfoCheckModal from "../../components/Signup/signupModal";
+import RoleSelect from "../../components/Signup/RoleSelect";
+import { setUserInfo } from "../../redux/Auth/authSlice";
 
 const UserInfoContainer = styled.div`
   font-family: ${(props) => props.theme.font.primary};
@@ -24,14 +28,22 @@ const NicknameInputBox = styled.input``;
 const NicknameCheckMessage = styled.p``;
 
 const UpdateUserInfoPage = () => {
+  const dispatch = useDispatch<AppDispatch>();
+  const navigate = useNavigate();
+
   const [nickname, setNickname] = useState(""); //input에서 받은 nickname
   const [nicknameCheckMessage, setNicknameCheckMessage] = useState(""); // 중복확인 결과 메세지
-  const [nicknameCheck, setNicknameCheck] = useState<boolean | null>(null); // 수정완료 시 중복확인 검사여부
+  const [nicknameCheck, setNicknameCheck] = useState<boolean | null>(null); // 중복확인 여부 확인
   const [lastNickname, setLastNickname] = useState(""); // api중복으로 안보내게
 
   const [selectedCategories, setSelectedCategories] = useState<
-    CategoryFormat[]
+    UserCategoriesList[]
   >([]); // 선택된 카테고리
+
+  const [selectedRole, setSelectedRole] = useState<boolean | null>(null); // role 선택상태
+
+  const [showSuccessModal, setShowSuccessModal] = useState(false); // 성공 모달
+  const [showFailModal, setShowFailModal] = useState(false); // 실패 모달
 
   const handleNicknameInput = (e: React.ChangeEvent<HTMLInputElement>) => {
     setNickname(e.target.value);
@@ -44,43 +56,47 @@ const UpdateUserInfoPage = () => {
   const checkNickname = async () => {
     if (nickname === "") {
       setNicknameCheckMessage("닉네임을 입력해주세요.");
+      setNicknameCheck(null); // 중복확인 다시 눌러야됨
       return;
-    } // 마지막 쓴 nickname과 같고, 체크도 되어있을 때
-    if (nickname === lastNickname && nicknameCheck !== null) {
-      setNicknameCheckMessage(
-        nicknameCheck
-          ? "사용가능한 닉네임입니다."
-          : "사용할 수 없는 닉네임입니다."
-      );
+    }
+    // 중복확인 연속 클릭 불가
+    if (nickname === lastNickname) {
+      setNicknameCheck(null);
       return;
     }
     try {
-      const result = await checkNicknameDuplicate(nickname);
-      console.log(result.data.isUsed);
+      const result = await checkNicknameAPI(nickname);
       setLastNickname(nickname);
-      if (result.data.isUsed === false) {
+      if (result.data.isUsed === true) {
         setNicknameCheck(false);
         setNicknameCheckMessage("사용할 수 없는 닉네임입니다.");
-      } else if (result.data.isUsed === true) {
+        console.log("isUsed: ", result.data.isUsed);
+      } else if (result.data.isUsed === false) {
         setNicknameCheck(true);
         setNicknameCheckMessage("사용가능한 닉네임입니다.");
+        console.log("isUsed: ", result.data.isUsed);
       }
     } catch (error) {
-      console.error("중복확인 오류:", error);
+      console.error("닉네임중복확인 오류:", error);
       setNicknameCheckMessage("오류가 발생했습니다. 다시 시도해주세요.");
     }
   };
 
   //카테고리 핸들러
   const handleCategorySelection = useCallback(
-    (categories: CategoryFormat[]) => {
+    (categories: UserCategoriesList[]) => {
       setSelectedCategories(categories);
     },
     []
   );
 
+  //role 선택 핸들러
+  const handleRoleSelection = (getDesireLecturer: boolean | null) => {
+    setSelectedRole(getDesireLecturer);
+  };
+
   //회원가입 완료 클릭
-  const InfoUpdateClick = async () => {
+  const infoUpdateClick = async () => {
     if (nickname.trim() == "") {
       alert("닉네임을 입력해주세요");
       return;
@@ -89,9 +105,54 @@ const UpdateUserInfoPage = () => {
       return;
     } else if (nicknameCheck === false) {
       alert("사용할 수 없는 닉네임입니다.");
-    } else if (selectedCategories.length === 0) {
-      alert("관심있는 카테고리를 선택해주세요.");
+      return;
+    } else if (nicknameCheck === true) {
+      try {
+        const result = await signupAPI(
+          nickname,
+          selectedCategories,
+          selectedRole
+        );
+
+        if (result.status === "OK") {
+          console.log("서버연결 성공");
+
+          const { memberId, nickname, role, getDesireLecturer, categories } =
+            result.data;
+
+          dispatch(
+            setUserInfo({
+              memberId: memberId,
+              nickname: nickname,
+              role: role,
+              getDesireLecturer: getDesireLecturer,
+              categories,
+            })
+          );
+          setNicknameCheck(true);
+          setShowSuccessModal(true);
+        } else {
+          setShowFailModal(true);
+          setNicknameCheck(false);
+          console.log("회원가입수정 실패");
+        }
+      } catch (error) {
+        console.error("회원가입수정 서버 오류 : ", error);
+      }
     }
+    return;
+  };
+
+  // 확인 누르면 변경된 회원수정페이지 보여주기
+  const handelConfirm = () => {
+    setShowSuccessModal(true);
+    navigate(PAGE_PATHS.USER_INFO);
+    setShowSuccessModal(false);
+  };
+
+  //취소 버튼 없어
+  const handelCancel = () => {
+    setShowFailModal(false);
   };
 
   return (
@@ -102,7 +163,7 @@ const UpdateUserInfoPage = () => {
         <NicknameInputBox
           value={nickname}
           onChange={handleNicknameInput}
-          placeholder="nickname"
+          placeholder={nickname}
         ></NicknameInputBox>
 
         <Button
@@ -115,12 +176,33 @@ const UpdateUserInfoPage = () => {
       </NicknameSection>
 
       <CategorySelect onCategoryChange={handleCategorySelection} />
+      <RoleSelect
+        onRoleChange={handleRoleSelection}
+        selectedRole={selectedRole}
+        styleType="checkbox"
+      />
 
       <Button
         text="회원정보수정완료"
-        onClick={InfoUpdateClick}
+        onClick={infoUpdateClick}
         design={2}
         fontWeight={400}
+      />
+
+      <InfoCheckModal
+        isOpen={showSuccessModal}
+        message="회원정보수정이 완료되었습니다."
+        onConfirm={handelConfirm}
+        onCancel={handelConfirm}
+        confirmText="확인"
+      />
+
+      <InfoCheckModal
+        isOpen={showFailModal}
+        message="회원가입에 실패했습니다."
+        onConfirm={handelCancel}
+        onCancel={handelCancel}
+        confirmText="확인"
       />
     </UserInfoContainer>
   );
