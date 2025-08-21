567e3ed393506aecfa34f9a73f0abf6acfec8650
Imdahee <ldhdh1832@gmail.com>
2025-08-21 16:32:21 +0900

    [LF-82]style : 페이지 디자인 수정

--- src/Pages/UserMypage/StudentLecturePage.tsx
+++ src/Pages/UserMypage/StudentLecturePage.tsx
@@ -4,9 +4,19 @@ import StudnetLectureContainer from "../../components/StudentLecture/StudentLect
 
 const StudentLectureSection = styled.div``;
 
+const StudentLecturePageSubtitle = styled.div`
+  border-bottom: 2px solid;
+  width: 1080px;
+  font-size: ${(props) => props.theme.fontSize.title.max};
+  font-family: ${(props) => props.theme.font.primary};
+  border-color: ${(props) => props.theme.colors.border_Dark};
+  margin: 0 auto 30px auto; /* 👈 좌우 auto로 가운데 정렬! */
+`;
+
 const StudentLecturePage = () => {
   return (
     <StudentLectureSection>
+      <StudentLecturePageSubtitle>MY PAGE</StudentLecturePageSubtitle>
       <StudnetLectureContainer />
     </StudentLectureSection>
   );
