0895cee4c1418738cd302899f4aeb7c84a374039
Lookies09 <khgyun09@gmail.com>
2025-08-22 10:27:41 +0900

    [LF-147] feat: 장바구니 담기 api 요청 구현

---
+++ src/api/Cart/cartAPI.ts
@@ -0,0 +1,27 @@
+// cartAPI
+import apiClient from "../index";
+import { API_ENDPOINTS } from "../../constants/endpoints";
+import { getErrorMessage } from "../../utils/handleApiError";
+
+interface PostCartProp {
+  lectureId: string;
+  title: string;
+  price: number;
+  thumbnailUrl: string | null | undefined;
+}
+
+export const postCartItem = async (
+  item: PostCartProp
+): Promise<PostCartProp> => {
+  try {
+    const response = await apiClient.post(
+      API_ENDPOINTS.PAYMENT.CART.POST,
+      item
+    );
+    return response.data;
+  } catch (error: unknown) {
+    const message = getErrorMessage(error, "장바구니 담기 실패");
+    console.error("Post Cart API error:", message);
+    throw new Error(message);
+  }
+};
