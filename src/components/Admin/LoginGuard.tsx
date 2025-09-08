6c5d9a4292bcf52fa892f9919589f503aa6393a8
Sunhokim2 <mcwright@naver.com>
2025-09-08 17:18:46 +0900

    hotfix: 비로그인 판명 조건수정

--- src/components/Admin/LoginGuard.tsx
+++ src/components/Admin/LoginGuard.tsx
@@ -14,7 +14,8 @@ const LoginGuard = ({ children }: LoginGuardProps) => {
     const memberId = useSelector((state: RootState) => state.auth.memberId);
 
     useEffect(() => {
-        if (!memberId) {
+        // 없는게 아니라 -1이면 비로그인 상태다.
+        if (memberId == -1) {
             alert("로그인시 이용가능합니다");
             navigate(PAGE_PATHS.LOGIN, { replace: true });
         }
