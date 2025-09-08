514cc072f799c74d201f03565fa90e908d0310e0
Sunhokim2 <mcwright@naver.com>
2025-09-08 16:44:47 +0900

    fix: 중복 장바구니 요청 받기 수정

--- src/components/LectureInfo/LectureInfoHeader.tsx
+++ src/components/LectureInfo/LectureInfoHeader.tsx
@@ -181,30 +181,20 @@ const LectureInfoHeader: React.FC<LectureHeaderProps> = ({
   const handleAddCart = async () => {
     try {
       if (!lecture) return;
-      const postResponse = await postCartItem({
+      await postCartItem({
         lectureId: lecture.lectureId,
         title: lecture.title,
         price: lecture.price,
         thumbnailUrl: lecture.thumbnail,
-      }).catch((err) => {
-        setModalMessage(err.response.data.message);
-        setIsSuccess(false);
-        setModalOpen(true);
-        return;
       });
-
-      if (!postResponse) {
-        setModalMessage("네트워크 오류 또는 서버에 접속할 수 없습니다.");
-        setIsSuccess(false);
-        setModalOpen(true);
-        return;
-      }
       setModalMessage("장바구니 담기 성공");
       setIsSuccess(true);
       setModalOpen(true);
-    } catch {
+    } catch (err: any) {
       // console.error(err);
-
+      setModalMessage(err.message || "에러 발생");
+      setIsSuccess(false);
+      setModalOpen(true);
     }
   };
 
@@ -257,8 +247,8 @@ const LectureInfoHeader: React.FC<LectureHeaderProps> = ({
                     link.click();
                     document.body.removeChild(link);
                     window.URL.revokeObjectURL(url);
-                  } catch (err) {
-                    console.error("파일 다운로드 실패", err);
+                  } catch {
+                    // console.error("파일 다운로드 실패", err);
                   }
                 }}
                 design={2}
