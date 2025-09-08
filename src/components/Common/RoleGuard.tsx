cfd9c7dde54dafca4c2ed537543f3d5a0f4d7420
Kim Hyeong Gyun <khgyun09@gmail.com>
2025-09-08 18:30:48 +0900

    fix: 빌드 오류 수정

--- src/components/Common/RoleGuard.tsx
+++ src/components/Common/RoleGuard.tsx
@@ -1,4 +1,4 @@
-import { useEffect, type ReactNode } from "react";
+import { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import type { RootState } from "../../redux/store";
