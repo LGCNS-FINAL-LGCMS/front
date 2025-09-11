6d35731f33d9b77cefbd747217af656e0af45321
Imdahee <ldhdh1832@gmail.com>
2025-09-11 15:13:07 +0900

    [LF-82]style: 사이드탭 디자인 수정

--- src/components/StudentSideBar/StudentSideBar.tsx
+++ src/components/StudentSideBar/StudentSideBar.tsx
@@ -1,11 +1,13 @@
 import { matchPath, useLocation, useNavigate } from "react-router-dom";
 import SideTab from "../Common/SideTab";
 import { PAGE_PATHS } from "../../constants/pagePaths";
+import { useState } from "react";
 
 const StudentSideBar = () => {
   const location = useLocation();
   const currentPath = location.pathname;
   const navigate = useNavigate();
+  const [id, setId] = useState<number>(0);
 
   // 사이드바가 보여질 경로 배열
   const visiblePaths = [
@@ -24,14 +26,16 @@ const StudentSideBar = () => {
   return (
     <SideTab
       title="마이페이지"
+      currentItemId={id}
       items={[
-        { id: 0, label: "나의 강의실" },
+        { id: 0, label: "나의 강좌" },
         { id: 1, label: "레벨 테스트" },
         { id: 2, label: "회원 정보 수정" },
         { id: 3, label: "나의 Q&A" },
       ]}
       onSelect={(id) => {
         const path = visiblePaths[id];
+        setId(id);
         if (path) navigate(path);
       }}
     />
